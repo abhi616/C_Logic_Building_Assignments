bd0a7683f360712e3ddfd4cc2bc89a2632f6adef
Abhi Yadav <71933892+abhi616@users.noreply.github.com>
2021-03-12 15:44:49 +0530

    Add files via upload

---
+++ Assignment 01/03)Program To Print 5 T0 1 Numbers On Screen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+void Display()
+{
+  int i=0;
+  i=5;
+  
+  while(i>0)
+   {
+     printf(" %d\n",i);
+     i--;
+   }
+  
+}
+
+int main()
+{
+      
+    Display();
+   
+   return 0;
+}
