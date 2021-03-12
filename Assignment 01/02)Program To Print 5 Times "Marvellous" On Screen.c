4834ae34847025a7d9ef1a1ad5ecb41250a03c02
Abhi Yadav <71933892+abhi616@users.noreply.github.com>
2021-03-12 15:44:17 +0530

    Add files via upload

---
+++ Assignment 01/02)Program To Print 5 Times "Marvellous" On Screen.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+void Display()
+{
+  int i=0;
+  
+  for(i=0;i<5;i++)
+   {
+     printf(" Marvellous\n");
+   }
+  
+}
+
+int main()
+{
+      
+    Display();
+   
+   return 0;
+}
