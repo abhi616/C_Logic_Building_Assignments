6012437a4c1653a3efe9b48b602d2f86ad1e3f96
Abhi Yadav <71933892+abhi616@users.noreply.github.com>
2021-03-17 00:46:04 +0530

    Add files via upload

---
+++ Assignment 06/03)Write a Program Which Accept Two Numbers From User And Check Whether That Numbers Are Equal Or Not.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+typedef int BOOL;
+
+#define TRUE 1
+#define FALSE 0
+
+BOOL ChkEqual(int iNo1,int iNo2)
+{
+  if(iNo1==iNo2)
+   {
+     return TRUE;
+   }
+  else
+   {
+     return FALSE;
+   }
+  
+}
+
+int main()
+{
+   int iValue1=0,iValue2=0;
+   BOOL bRet=FALSE;
+   
+   printf("Enter Number: ");
+   scanf("%d%d",&iValue1,&iValue2);
+   
+   bRet=ChkEqual(iValue1,iValue2);
+   
+   if(bRet==TRUE)
+    {
+      printf("\nEqual\n");
+    }
+   else
+    {
+      printf("\nNot Equal\n");
+    }
+ 
+   return 0;
+}
