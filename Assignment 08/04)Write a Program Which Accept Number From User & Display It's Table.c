1dcf0a92834325acc3adc5d0dd2bec97ec223379
Abhi Yadav <71933892+abhi616@users.noreply.github.com>
2021-03-19 12:39:03 +0530

    Add files via upload

---
+++ Assignment 08/04)Write a Program Which Accept Number From User & Display It's Table.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+void Table(int iNo)
+{
+  int i=1;
+      
+      //To Handle Negative Input
+      if(iNo<0)
+      {
+        iNo=-iNo;
+      }
+      
+      for(i;i<=10;i++)
+      {
+        printf("%d ",iNo*i);
+      }
+      
+        
+  return;
+}
+
+int main()
+{
+   int iValue=0;
+   
+  printf("Enter Number: ");
+  scanf("%d",&iValue);
+  
+  Table(iValue);
+  
+  return 0;
+}
