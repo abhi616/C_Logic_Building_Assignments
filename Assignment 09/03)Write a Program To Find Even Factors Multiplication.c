182ce6bea92f6a3ee8944acace7197031f8ef2cd
Abhi Yadav <71933892+abhi616@users.noreply.github.com>
2021-03-20 09:43:10 +0530

    Add files via upload

---
+++ Assignment 09/03)Write a Program To Find Even Factors Multiplication.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int Even_Factors_Mult(int iNo)
+{
+   int Fact=1;
+     
+      if(iNo<0)
+      {
+        iNo=-iNo;
+      }
+      
+      for(;iNo>0;iNo--)
+       {
+         if(iNo%2==0)
+          {
+            Fact=Fact*iNo;
+          }
+       }
+                       
+  return Fact;  
+}
+
+int main()
+{
+   int iValue=0,iRet=0;
+   
+  printf("Enter Number: ");
+  scanf("%d",&iValue);
+  
+  iRet=Even_Factors_Mult(iValue);
+  
+  printf("\nEven Factorials Multiplication Is = %d\n",iRet);
+  
+  return 0;
+}
