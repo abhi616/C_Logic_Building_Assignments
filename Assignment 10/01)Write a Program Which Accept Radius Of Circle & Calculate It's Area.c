209c13e5c052b2a0950105f4af36d943577df3d3
Abhi Yadav <71933892+abhi616@users.noreply.github.com>
2021-03-21 09:22:26 +0530

    Add files via upload

---
+++ Assignment 10/01)Write a Program Which Accept Radius Of Circle & Calculate It's Area.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#define PI 3.14
+
+//Compiler version gcc  6.3.0
+
+double Circle_Area(float fRadius)
+{
+   double Area=0.0;
+     
+   Area=PI*fRadius*fRadius;  //PI=3.14
+                             
+  return Area;  
+}
+
+int main()
+{
+   float fValue=0.0;
+   double dRet=0.0;
+   
+  printf("Enter Radius: ");
+  scanf("%f",&fValue);
+  
+  dRet=Circle_Area(fValue);
+  
+  printf("\nArea Of Circle Is = %lf\n",dRet);
+  
+  return 0;
+}
