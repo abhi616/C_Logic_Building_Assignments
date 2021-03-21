30c0dd9cd60fa92aa899677668de967c27f2988e
Abhi Yadav <ay891241@gmail.com>
2021-03-21 10:32:21 +0530

    Add files via upload

---
+++ Assignment 10/04)Accept Temperature In Fahrenheit And Convert It Into Celcius.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+//Compiler version gcc  6.3.0
+
+double FhToCs(float fTemp)
+{
+   float Cel=0.0;
+     
+   Cel=(fTemp-32)*(0.555);  
+                    
+  return Cel;  
+}
+
+int main()
+{
+   float fValue=0.0;
+   double dRet=0.0;
+   
+  printf("Enter Temperature In Fahrenheit: ");
+  scanf("%f",&fValue);
+  
+  dRet=FhToCs(fValue);
+  
+  printf("\nTemperature In Celcius Is = %.3f\n",dRet);
+  
+  return 0;
+}
