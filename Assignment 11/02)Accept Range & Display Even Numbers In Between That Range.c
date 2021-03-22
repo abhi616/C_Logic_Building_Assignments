33ad1594a465ad68696580997aca9cf3a3428cf6
Abhi Yadav <ay891241@gmail.com>
2021-03-22 10:37:54 +0530

    Add files via upload

---
+++ Assignment 11/02)Accept Range & Display Even Numbers In Between That Range.c
@@ -0,0 +1,40 @@
+/*
+    Input:23 35
+    Output:24 26 28 30 32 34
+*/
+
+
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+void RangeDisplayEven(int iStart,int iEnd)
+{
+  
+   for(;iStart<=iEnd;iStart++)
+   {
+     
+     if(iStart%2==0)
+      {
+        printf("%d ",iStart);
+      }
+     
+   }
+ 
+  return;
+}
+
+int main()
+{
+  int iValue1=0,iValue2=0;
+  
+  printf("Enter Starting Point: ");
+  scanf("%d",&iValue1);
+  
+  printf("\nEnter Ending Point: ");
+  scanf("%d",&iValue2);
+  
+  RangeDisplayEven(iValue1,iValue2);
+  
+  return 0;
+}
