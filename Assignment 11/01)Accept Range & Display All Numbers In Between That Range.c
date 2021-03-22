46e9eb5e8ceed40eb691701a9de777facf1428a1
Abhi Yadav <ay891241@gmail.com>
2021-03-22 09:36:28 +0530

    Add files via upload

---
+++ Assignment 11/01)Accept Range & Display All Numbers In Between That Range.c
@@ -0,0 +1,35 @@
+/*
+    Input:23 35
+    Output:23 24 26 27 28 29 30 31 32 33 34 35
+*/
+
+
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+void RangeDisplay(int iStart,int iEnd)
+{
+  
+   for(;iStart<=iEnd;iStart++)
+   {
+     printf("%d ",iStart);
+   }
+ 
+  return;
+}
+
+int main()
+{
+  int iValue1=0,iValue2=0;
+  
+  printf("Enter Starting Point: ");
+  scanf("%d",&iValue1);
+  
+  printf("\nEnter Ending Point: ");
+  scanf("%d",&iValue2);
+  
+  RangeDisplay(iValue1,iValue2);
+  
+  return 0;
+}
