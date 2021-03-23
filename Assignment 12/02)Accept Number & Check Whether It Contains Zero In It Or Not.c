9682c48ac63171f7e2423e28f2dfe130de8e5931
Abhi Yadav <ay891241@gmail.com>
2021-03-23 09:39:17 +0530

    Add files via upload

---
+++ Assignment 12/02)Accept Number & Check Whether It Contains Zero In It Or Not.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+typedef int BOOL;
+
+#define TRUE 1
+#define FALSE 0
+
+//Compiler version gcc  6.3.0
+
+BOOL ChkZero(int iNo)
+{
+   int Rem=0;
+   
+   if(iNo<0)
+    {
+      iNo=-iNo;
+    }
+  
+   for(;iNo>0;iNo=iNo/10)
+    {
+      Rem=iNo%10;
+      
+      if(Rem==0)  
+       {
+         return TRUE;
+       }  
+         
+    }
+       
+   return FALSE;
+}
+
+int main()
+{
+  int iValue=0;
+  BOOL bRet=FALSE;
+  
+  printf("Enter Number: ");
+  scanf("%d",&iValue);
+  
+  bRet=ChkZero(iValue);
+  
+  if(bRet==TRUE)
+   {
+      printf("\nIt Contains Zero\n");
+   }
+  else
+   {
+     printf("\nThere Is No Zero\n");
+   }
+  
+  return 0;
+}
