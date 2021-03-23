126a0bff047863d4c46e3a7c0c6465ecc273ef06
Abhi Yadav <ay891241@gmail.com>
2021-03-23 10:49:25 +0530

    Add files via upload

---
+++ Assignment 12/05)Accept Number & Count Frequency Of Such a Digits Which Are Less Than 6.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+
+//Compiler version gcc  6.3.0
+
+int Count(int iNo)
+{
+   int Rem=0,Cnt=0;
+   
+   if(iNo<0)
+    {
+      iNo=-iNo;
+    }
+  
+   for(;iNo>0;iNo=iNo/10)
+    {
+      Rem=iNo%10;
+      
+      if(Rem<6)  
+       {
+         Cnt++;
+       }  
+         
+    }
+       
+   return Cnt;
+}
+
+int main()
+{
+  int iValue=0,iRet=0;
+  
+  printf("Enter Number: ");
+  scanf("%d",&iValue);
+  
+  iRet=Count(iValue);
+  
+  
+  printf("\nCount Of Digits Which Are Less Than 6 = %d\n",iRet);
+   
+  
+  return 0;
+}
