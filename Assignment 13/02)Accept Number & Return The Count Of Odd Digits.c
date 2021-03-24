bbe5709788a08c601f4121ed4ee1be816d3c284c
Abhi Yadav <ay891241@gmail.com>
2021-03-24 20:58:56 +0530

    Add files via upload

---
+++ Assignment 13/02)Accept Number & Return The Count Of Odd Digits.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+
+//Compiler version gcc  6.3.0
+
+int OddCount(int iNo)
+{
+   int Rem=0,OCnt=0;
+   
+   if(iNo<0)
+    {
+      iNo=-iNo;
+    }
+  
+   for(;iNo>0;iNo=iNo/10)
+    {
+      Rem=iNo%10;
+      
+      if(Rem%2!=0)  
+       {
+         OCnt++;
+       }  
+         
+    }
+       
+   return OCnt;
+}
+
+int main()
+{
+  int iValue=0,iRet=0;
+  
+  printf("Enter Number: ");
+  scanf("%d",&iValue);
+  
+  iRet=OddCount(iValue);
+  
+  
+  printf("\nCount Of Odd Digits = %d\n",iRet);
+   
+  
+  return 0;
+}
