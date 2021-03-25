aa2c0f08ee0b66fb9560d54f6d39866f10b9b8ce
Abhi Yadav <ay891241@gmail.com>
2021-03-25 12:05:05 +0530

    Add files via upload

---
+++ Assignment 14/01)Accept Number & Display Below Pattern.c
@@ -0,0 +1,35 @@
+/*
+  Input : 5 
+  Output : A B C D E
+*/
+
+
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+void Pattern(int iNo)
+{
+  
+   int i=0;
+   char Ch="\0";
+   
+   for(Ch='A';i<iNo;i++,Ch++)
+    {
+      printf("%c ",Ch);
+    }
+  
+   return;
+}
+
+int main()
+{
+  int iValue=0;
+  
+  printf("Enter Number: ");
+  scanf("%d",&iValue);
+  
+  Pattern(iValue);
+  
+  return 0;
+}
