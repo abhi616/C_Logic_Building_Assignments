2ed1c85c24fa268b78f16de706d184eb0e8858c2
Abhi Yadav <ay891241@gmail.com>
2021-03-30 10:30:32 +0530

    Add files via upload

---
+++ Assignment 19/01)Accept Number Of Rows& Columns and Display Below Pattern.c
@@ -0,0 +1,43 @@
+/*
+    Input : iRow = 4 iCol = 4 
+    Output : 
+             * 
+             *  * 
+             *  *  * 
+             *  *  *  *
+*/
+
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+void Pattern(int iRow,int iCol)
+{
+  int i,j;
+  
+  for(i=0;i<iRow;i++)
+   {
+     for(j=0;j<iCol;j++)
+      {
+        if(i>=j)
+         {
+           printf("*  ");
+         }
+      }
+      
+      printf("\n");
+   }
+  
+}
+
+int main()
+{
+  int iValue1,iValue2;
+  
+  printf("Enter Number Of Rows & Columns: ");
+  scanf("%d%d",&iValue1,&iValue2);
+  
+  Pattern(iValue1,iValue2);
+  
+  return 0;
+}
