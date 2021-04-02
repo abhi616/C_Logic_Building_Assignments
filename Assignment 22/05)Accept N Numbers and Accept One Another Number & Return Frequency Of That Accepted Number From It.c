a70d6f6acdf27fdb62b985312504654346bed60a
Abhi Yadav <ay891241@gmail.com>
2021-04-02 23:48:49 +0530

    Add files via upload

---
+++ Assignment 22/05)Accept N Numbers and Accept One Another Number & Return Frequency Of That Accepted Number From It.c
@@ -0,0 +1,83 @@
+/* 
+      Input : iSize = 6 
+      
+      NO = 66 
+      
+      Elements : 85 66 3 66 93 88
+   
+      Output : 2 
+
+
+      Input : iSize = 6
+      
+      No = 12 
+ 
+      Elements : 85 11 3 15 11 111
+      
+      Output : 0
+ */
+
+
+#include <stdio.h>
+#include<conio.h>
+
+//Compiler version gcc  6.3.0
+
+int Frequency(int Arr[],int iLength,int iNo)
+{
+  int i=0,Cnt=0;  
+      
+   for(i=0;i<iLength;i++)
+   {
+     if(Arr[i]<=0)
+     {
+       continue;
+     }
+     
+     if(Arr[i]==iNo)
+      {        
+       Cnt++;
+      }
+    
+   }        
+    
+   return Cnt;
+}
+
+int main()
+{
+  int iSize=0,iCnt=0,iRet=0,iValue=0;
+  int *P=NULL; 
+    
+  printf("Enter Number Of Elements: ");
+  scanf("%d",&iSize);
+  
+  printf("\nEnter The Number : ");
+  scanf("%d",&iValue);
+  
+  P=(int*)malloc(iSize*sizeof(int));
+  
+  if(P==NULL)
+   {
+     printf("\nUnable To Allocate Memory.");
+     return -1;
+   }      
+   
+   printf("\nEnter %d Elements: \n",iSize);  
+   
+   for(iCnt=0;iCnt<iSize;iCnt++)
+    {      
+       printf("\n•Enter Element %d : ",iCnt+1);
+       scanf("%d",&P[iCnt]);              
+    }
+   
+   iRet=Frequency(P,iSize,iValue);
+     
+   printf("\nFrequency Of %d = %d\n",iValue,iRet);
+       
+   free(P);
+    
+   getch();
+   
+   return 0;
+}
