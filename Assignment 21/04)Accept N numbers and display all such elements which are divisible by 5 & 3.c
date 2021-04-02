b67d3c66af43c4f554b3185de5a4295df4df7535
Abhi Yadav <ay891241@gmail.com>
2021-04-02 22:43:27 +0530

    Add files via upload

---
+++ Assignment 21/04)Accept N numbers and display all such elements which are divisible by 5 & 3.c
@@ -0,0 +1,72 @@
+/* 
+      Input : iSize = 6 
+  
+      Elements : 85 66 3 15 93 88
+  
+      Output : 15
+ */
+
+
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+void Display(int Arr[],int iLength)
+{
+  int i=0,Cnt=0;  
+   
+  printf("\nNumbers Which Are Divisible By 5 & 3:\n");
+   
+   for(i=0;i<iLength;i++)
+   {
+     if(Arr[i]<=0)
+     {
+       continue;
+     }
+     
+     if(Arr[i]%5==0&&Arr[i]%3==0)
+      {
+        printf("%d ",Arr[i]);
+        Cnt=1;
+      }
+     
+   }   
+   
+   if(Cnt==0)    //For Handling Such Cases Where Number Isn't Divisible By 5
+    {
+      printf("\nDoesn't Contain Any Element Which Are Divisible By 5&3...!\n");
+    }
+    
+   return;
+}
+
+int main()
+{
+  int iSize=0,iCnt=0;
+  int *P=NULL;
+  
+  printf("Enter Number Of Elements: ");
+  scanf("%d",&iSize);
+  
+  P=(int*)malloc(iSize*sizeof(int));
+  
+  if(P==NULL)
+   {
+     printf("\nUnable To Allocate Memory.");
+     return -1;
+   }      
+   
+   printf("\nEnter %d Elements: \n",iSize);  
+   
+   for(iCnt=0;iCnt<iSize;iCnt++)
+    {      
+       printf("\n•Enter Element %d : ",iCnt+1);
+       scanf("%d",&P[iCnt]);              
+    }
+   
+    Display(P,iSize);
+           
+    free(P);
+    
+  return 0;
+}
