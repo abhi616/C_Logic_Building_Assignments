fc15fcdcd8d2b6ae2efccee75526187bef171f36
Abhi Yadav <ay891241@gmail.com>
2021-04-04 23:25:05 +0530

    Add files via upload

---
+++ Assignment 24/02)Accept N Number And Return The Smallest Number.c
@@ -0,0 +1,70 @@
+/*
+     Input : iSize = 6 
+   
+     Elements : 85 66 3 66 93 88 
+ 
+     Output : 3
+*/
+
+
+#include <stdio.h>
+#include<conio.h>
+
+//Compiler version gcc  6.3.0
+
+int Minimum(int Arr[],int iLength)
+{
+   int i=0,Min=0;
+   
+   for(i=0;i<iLength;i++)
+    {
+      
+      if(i==0)
+      {
+        Min=Arr[i];
+      }
+      
+     if(Arr[i]<Min)
+      {
+        Min=Arr[i];
+      }
+      
+    }
+  
+  
+  return Min;
+}
+
+int main()
+{
+  int iSize=0,iCnt=0,iRet=0;
+  int *P=NULL;
+  
+  printf("Enter Number Of Elements: ");
+  scanf("%d",&iSize);
+  
+  P=(int*)malloc(iSize*sizeof(int));
+  
+  if(P==NULL)
+  {
+    printf("\nUnable To Allocate Memory...!");
+    return -1;
+  }
+  
+  printf("\nEnter %d Elements: \n",iSize);
+  
+  for(iCnt=0;iCnt<iSize;iCnt++)
+   {
+     printf("\nEnter %d Element: ",iCnt+1);
+     scanf("%d",&P[iCnt]);     
+   }
+   
+   iRet=Minimum(P,iSize);
+   
+   printf("\nSmallest Element From Entered Elements = %d\n",iRet);
+   
+   free(P);
+   
+   getch();
+   return 0;  
+}
