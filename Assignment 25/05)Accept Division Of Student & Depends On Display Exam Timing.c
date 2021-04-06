cb9e4e2b8e6fb7817dfd4acdb1d796bcfc1e4943
Abhi Yadav <ay891241@gmail.com>
2021-04-06 20:39:07 +0530

    Add files via upload

---
+++ Assignment 25/05)Accept Division Of Student & Depends On Display Exam Timing.c
@@ -0,0 +1,61 @@
+/*
+    Input : C 
+    Output : Your exam at 9.20 AM 
+
+    Input : d 
+    Output : Your exam at 10.30 AM
+*/
+
+
+#include <stdio.h>
+#include<conio.h>
+
+//Compiler version gcc  6.3.0
+
+
+void DisplaySchedule(char ChDiv)
+{
+  
+   switch(ChDiv)
+   {
+     case 'A':
+     case 'a':
+              printf("\nYour exam at 7 AM\n");
+              break;
+              
+     case 'B':
+     case 'b':
+              printf("\nYour exam at 8:30 Am\n");
+              break;
+     
+     case 'C':
+     case 'c':
+              printf("\nYour exam at 9:20 AM\n");
+              break;
+              
+     case 'D':
+     case 'd':
+              printf("\nYour exam at 10:30 AM\n");
+              break;
+     default:
+             printf("\nYou Entered Invalid Division...!\n");
+             break;
+                                     
+   }
+   
+   return;
+}
+
+int main()
+{
+  char cValue='\0';  
+  
+  printf("Enter Your Division: ");
+  scanf("%c",&cValue);
+  
+  DisplaySchedule(cValue);
+  
+  getch();
+  
+  return 0;
+}
