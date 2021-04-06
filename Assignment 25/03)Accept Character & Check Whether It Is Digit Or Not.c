902283ba59f002ee171cff80b38e0d70430aa83d
Abhi Yadav <ay891241@gmail.com>
2021-04-06 17:20:54 +0530

    Add files via upload

---
+++ Assignment 25/03)Accept Character & Check Whether It Is Digit Or Not.c
@@ -0,0 +1,55 @@
+/*
+    Input : 7 
+    Output : TRUE 
+
+    Input : d 
+    Output : FALSE
+*/
+
+
+#include <stdio.h>
+#include<conio.h>
+
+//Compiler version gcc  6.3.0
+
+typedef int BOOL;
+
+#define TRUE 1
+#define FALSE 0
+
+BOOL ChkDigit(char Ch)
+{
+   if(Ch>=48&&Ch<=57)
+   {
+     return TRUE;
+   }
+   else
+   {
+     return FALSE;
+   }
+   
+}
+
+int main()
+{
+  char cValue='\0';
+  BOOL bRet=FALSE;
+  
+  printf("Enter Character: ");
+  scanf("%c",&cValue);
+  
+  bRet=ChkDigit(cValue);
+  
+  if(bRet==TRUE)
+  {
+    printf("\nIt Is Digit\n");
+  }
+  else
+  {
+    printf("\nIt Is Not Digit\n");
+  }
+  
+  getch();
+  
+  return 0;
+}
