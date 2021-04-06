e2224ade077fbe3921d34606cddfba06faf76eda
Abhi Yadav <ay891241@gmail.com>
2021-04-06 17:18:29 +0530

    Add files via upload

---
+++ Assignment 25/02)Accept Character & Check Whether It Is Capital Or Not.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include<conio.h>
+//Compiler version gcc  6.3.0
+
+typedef int BOOL;
+
+#define TRUE 1
+#define FALSE 0
+
+BOOL ChkCapital(char Ch)
+{
+   if(Ch>=65&&Ch<=90)
+   {
+     return TRUE;
+   }
+   else
+   {
+     return FALSE;
+   }
+   
+}
+
+int main()
+{
+  char cValue='\0';
+  BOOL bRet=FALSE;
+  
+  printf("Enter Character: ");
+  scanf("%c",&cValue);
+  
+  bRet=ChkCapital(cValue);
+  
+  if(bRet==TRUE)
+  {
+    printf("\nIt Is Capital\n");
+  }
+  else
+  {
+    printf("\nIt Is Not Capital\n");
+  }
+  
+  getch();
+  
+  return 0;
+}
