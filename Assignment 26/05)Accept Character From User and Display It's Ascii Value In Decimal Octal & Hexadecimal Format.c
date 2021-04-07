1a67ac83303944efca275df1f1bd4ebde0d90e4c
Abhi Yadav <ay891241@gmail.com>
2021-04-07 12:55:56 +0530

    Twenty Sixth Assignment-05

---
+++ Assignment 26/05)Accept Character From User and Display It's Ascii Value In Decimal Octal & Hexadecimal Format.c
@@ -0,0 +1,35 @@
+/*
+     Input : A
+     Output : Decimal  65
+     
+              Octal 0101 
+              
+              Hexadecimal 0X41     
+*/ 
+
+
+#include <stdio.h>
+#include<conio.h>
+
+//Compiler version gcc  6.3.0
+
+void Display(char Ch)
+{ 
+   printf("\nChar | Decimal | Octal | Hexadecimal \n %-3c | %-7d | %-5o | %-5x\n",Ch,Ch,Ch,Ch);
+    
+   return;   
+}
+
+int main()
+{  
+  char cValue='\0'; 
+  
+  printf("Enter Character: ");
+  scanf("%c",&cValue);
+  
+  Display(cValue);  
+      
+  getch();
+  
+  return 0;
+}
