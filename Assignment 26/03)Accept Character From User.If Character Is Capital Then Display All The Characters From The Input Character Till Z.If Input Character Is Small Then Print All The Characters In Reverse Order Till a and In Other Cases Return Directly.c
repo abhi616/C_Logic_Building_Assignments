8372cef4739960b9cf7229e578d262c8a0b0621a
Abhi Yadav <ay891241@gmail.com>
2021-04-07 12:53:30 +0530

    Twenty Sixth Assignment-03

---
+++ Assignment 26/03)Accept Character From User.If Character Is Capital Then Display All The Characters From The Input Character Till Z.If Input Character Is Small Then Print All The Characters In Reverse Order Till a and In Other Cases Return Directly.c
@@ -0,0 +1,52 @@
+/*
+     Input : Q 
+     Output : Q R S T U V W X Y Z
+
+     Input : m 
+     Output : m l k j i h g f e d c b a
+
+     Input : 8
+     Output :  
+*/ 
+
+
+#include <stdio.h>
+#include<conio.h>
+
+//Compiler version gcc  6.3.0
+
+void Display(char Ch)
+{ 
+   if(Ch>=65&&Ch<=90)
+    {
+      for(;Ch<='Z';Ch++)
+       {
+         printf("%c ",Ch);
+       }
+      
+    }
+    
+   else if(Ch>=97&&Ch<=122)
+    {
+      for(;Ch>='a';Ch--)
+       {
+         printf("%c ",Ch);
+       }
+    }
+    
+   return;
+}
+
+int main()
+{  
+  char cValue='\0';
+  
+  printf("Enter Character: ");
+  scanf("%c",&cValue);
+  
+  Display(cValue);  
+  
+  getch();
+  
+  return 0;
+}
