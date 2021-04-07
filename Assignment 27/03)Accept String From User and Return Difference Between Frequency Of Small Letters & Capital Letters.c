18308f501cc08da51080a0d847c1c484adf0f958
Abhi Yadav <ay891241@gmail.com>
2021-04-07 17:05:44 +0530

    Add files via upload

---
+++ Assignment 27/03)Accept String From User and Return Difference Between Frequency Of Small Letters & Capital Letters.c
@@ -0,0 +1,48 @@
+/*
+     Input : “MarvellouS”
+     Output : 6    (8-2)
+*/ 
+
+
+#include <stdio.h>
+#include<conio.h>
+
+//Compiler version gcc  6.3.0
+
+int Difference(char *Str)
+{ 
+   int C_Cnt=0,S_Cnt=0;
+   
+   while(*Str!='\0')
+    {
+      if(*Str>=65&&*Str<=90)
+       {
+         C_Cnt++;
+       }
+      else
+       {
+         S_Cnt++;
+       }
+       
+      Str++;
+    }
+    
+   return S_Cnt-C_Cnt;   
+}
+
+int main()
+{  
+  char Arr[20]; 
+  int iRet=0;
+  
+  printf("Enter String: ");
+  scanf("%[^'\n']s",&Arr);
+  
+  iRet=Difference(Arr);
+   
+  printf("\nSmall Letters & Capital Count Difference = %d\n",iRet);
+     
+  getch();
+  
+  return 0;
+}
