2d59077c143819af9ab469be7ff1d8dbf1f146e9
Abhi Yadav <ay891241@gmail.com>
2021-04-07 17:02:43 +0530

    Twenty Seventh Assignment

---
+++ Assignment 27/01)Accept String From User and Count Number Of Capital Letters.c
@@ -0,0 +1,44 @@
+/*
+     Input : “Marvellous Multi OS”
+     Output : 4    
+*/ 
+
+
+#include <stdio.h>
+#include<conio.h>
+
+//Compiler version gcc  6.3.0
+
+int CountCapital(char *Str)
+{ 
+   int iCnt=0;
+   
+   while(*Str!='\0')
+    {
+      if(*Str>=65&&*Str<=90)
+       {
+         iCnt++;
+       }
+       
+      Str++;
+    }
+    
+   return iCnt;   
+}
+
+int main()
+{  
+  char Arr[20]; 
+  int iRet=0;
+  
+  printf("Enter Character: ");
+  scanf("%[^'\n']s",&Arr);
+  
+  iRet=CountCapital(Arr);
+   
+  printf("\nCapital Letters Count = %d\n",iRet);
+     
+  getch();
+  
+  return 0;
+}
