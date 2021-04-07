e033ab101a61d0cb56ec2f43cf36e1d95583cf20
Abhi Yadav <ay891241@gmail.com>
2021-04-07 12:55:11 +0530

    Twenty Sixth Assignment-04

---
+++ Assignment 26/04)Accept Character From User and Check Whether It Is Special Symbol Or Not.c
@@ -0,0 +1,56 @@
+/*
+     Input : % 
+     Output : TRUE
+
+     Input : d
+     Output : FALSE
+*/ 
+
+
+#include <stdio.h>
+#include<conio.h>
+
+//Compiler version gcc  6.3.0
+
+typedef int BOOL;
+
+#define TRUE 1
+#define FALSE 0
+
+
+BOOL ChkSpecial(char Ch)
+{ 
+   if(!((Ch>=65&&Ch<=90)||(Ch>=97&&Ch<=122)||(Ch>=48&&Ch<=57)))
+    {     
+      return  TRUE;           
+    }    
+   else 
+    {
+      return FALSE;
+    }
+       
+}
+
+int main()
+{  
+  char cValue='\0';
+  BOOL bRet=FALSE;
+  
+  printf("Enter Character: ");
+  scanf("%c",&cValue);
+  
+ bRet=ChkSpecial(cValue);  
+  
+  if(bRet==TRUE)
+   {
+     printf("\nIt Is Special Character/Symbol\n");
+   }
+  else
+   {
+     printf("\nIt Is Not a Special Character/Symbol\n");
+   }
+   
+  getch();
+  
+  return 0;
+}
