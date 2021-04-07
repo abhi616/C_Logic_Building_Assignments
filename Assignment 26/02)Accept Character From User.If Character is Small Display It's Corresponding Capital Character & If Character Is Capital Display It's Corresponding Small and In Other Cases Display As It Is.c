d0d23353d77fc5d53186d9f6d336a5699babfb1f
Abhi Yadav <ay891241@gmail.com>
2021-04-07 12:50:35 +0530

    Twenty Sixth Assignment-02

---
+++ Assignment 26/02)Accept Character From User.If Character is Small Display It's Corresponding Capital Character & If Character Is Capital Display It's Corresponding Small and In Other Cases Display As It Is.c
@@ -0,0 +1,48 @@
+/*
+     Input : Q 
+     Output : q 
+
+     Input : m 
+     Output : M 
+
+     Input : 4 
+     Output : 4 
+*/ 
+
+
+#include <stdio.h>
+#include<conio.h>
+
+//Compiler version gcc  6.3.0
+
+void Display(char Ch)
+{ 
+   if(Ch>=65&&Ch<=90)
+    {
+      printf("\nCharacter = %c\n",Ch+32);
+    }
+   else if(Ch>=97&&Ch<=122)
+    {
+      printf("\nCharacter = %c\n",Ch-32);
+    }
+   else
+    {
+      printf("\nCharacter = %c\n",Ch);
+    }
+    
+   return;
+}
+
+int main()
+{  
+  char cValue='\0';
+  
+  printf("Enter Character: ");
+  scanf("%c",&cValue);
+  
+  Display(cValue);  
+  
+  getch();
+  
+  return 0;
+}
