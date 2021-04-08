ff4b4b86a9bb4d17044fb9b49b5c0f9acc59b762
Abhi Yadav <ay891241@gmail.com>
2021-04-08 14:18:47 +0530

    Add files via upload

---
+++ Assignment 28/02)Accept String From User & Convert It Into Upper Case.c
@@ -0,0 +1,44 @@
+/*
+  
+     Input : “Marvellous Multi OS” 
+     
+     Output : MARVELLOUS MULTI OS
+
+*/
+
+#include <stdio.h>
+#include<conio.h>
+
+//Compiler version gcc  6.3.0
+
+void Struprx(char *Str)
+{
+   for(;*Str!='\0';Str++)
+    {
+      if(*Str>=97&&*Str<=122)
+       {
+         *Str-=32;
+       }
+      else
+       {
+         continue;
+       }
+    }
+    
+    return;
+}
+
+int main()
+{
+  char Arr[30];
+  
+  printf("Enter String: ");
+  scanf("%[^'\n']s",&Arr);
+  
+  Struprx(Arr);
+  
+  printf("\nModified String = %s\n",Arr);
+  
+  getch();
+  return 0;
+}
