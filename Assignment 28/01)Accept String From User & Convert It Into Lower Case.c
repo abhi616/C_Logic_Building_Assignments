706a63e1cf4d88a93562e9b33fbd45cc8016d966
Abhi Yadav <ay891241@gmail.com>
2021-04-08 14:21:42 +0530

    Twenty Eight Assignment

---
+++ Assignment 28/01)Accept String From User & Convert It Into Lower Case.c
@@ -0,0 +1,44 @@
+/*
+  
+     Input : “Marvellous Multi OS” 
+     
+     Output : marvellous multi os
+
+*/
+
+#include <stdio.h>
+#include<conio.h>
+
+//Compiler version gcc  6.3.0
+
+void Strlwrx(char *Str)
+{
+   for(;*Str!='\0';Str++)
+    {
+      if(*Str>=65&&*Str<=90)
+       {
+         *Str+=32;
+       }
+      else
+       {
+         continue;
+       }
+    }
+    
+    return;
+}
+
+int main()
+{
+  char Arr[30];
+  
+  printf("Enter String: ");
+  scanf("%[^'\n']s",&Arr);
+  
+  Strlwrx(Arr);
+  
+  printf("\nModified String = %s\n",Arr);
+  
+  getch();
+  return 0;
+}
