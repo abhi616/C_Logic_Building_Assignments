da17a8ae675f29fc11e9b7821b1660fd41e7efe7
Abhi Yadav <ay891241@gmail.com>
2021-04-12 12:32:36 +0530

    Add files via upload

---
+++ Assignment 31/02)Program To Copy One String Into Another String By Removing All White Spaces.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include<conio.h>
+#include<string.h>
+
+//Compiler version gcc  6.3.0
+
+void StrCpyRev(char *Src,char *Dest)
+{
+
+  while(*Src!='\0')   
+  {
+    if(*Src==' ')
+    {
+      *Src++;
+      continue;
+    }
+        
+    *Dest=*Src;
+    Src++;
+    Dest++;
+     
+  }     
+  
+  *Dest='\0';
+  
+  return;
+}
+
+int main()
+{
+  char Str1[30]="Marv  ellous ";
+  char Str2[30]="Marvellous";
+  
+  StrCpyRev(Str1,Str2);
+ 
+  printf("\nSource String = %s\n",Str1);
+  printf("\nCopied String = %s\n",Str2);
+  
+  getch();
+  return 0;
+}
