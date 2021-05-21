b86c2c1dc11e2021c4bb7c272ae4fd79b08916e9
Abhi Yadav <ay891241@gmail.com>
2021-05-21 23:20:59 +0530

    Add files via upload

---
+++ Assignment 38/03)String Pattern-3.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include<conio.h>
+
+//Compiler version gcc  6.3.0
+
+void String_Pattern(char *pStr)
+{
+  int L=0,i=0,j=0;
+  
+  for(L=0;pStr[L]!='\0';L++);
+  
+  printf("\n");
+  
+  for(i=0;i<L;i++)
+  {
+    for(j=0;j<L;j++)
+    {
+      /*
+          //If We Want To Skip/Avoid White Spaces Then Use This if.
+
+       if(pStr[j]==' ')
+       {
+         continue;
+       }
+        
+      */
+        
+      if(i>=j)
+      {
+        printf("%c  ",pStr[j]);
+      }
+    }
+    
+    printf("\n");
+  }
+  
+  
+  return;
+}
+
+int main()
+{
+  char Str[30]="\0";
+  
+  printf("Enter String : ");
+  scanf("%[^\n]s",&Str);
+  
+  String_Pattern(Str);
+  
+  getch();
+  return 0;
+}
