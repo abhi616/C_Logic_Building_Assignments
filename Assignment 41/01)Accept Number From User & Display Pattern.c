900b0d0242140ce1ac75c97322833dfab903fe76
Abhi Yadav <ay891241@gmail.com>
2021-05-25 23:28:25 +0530

    Add files via upload

---
+++ Assignment 41/01)Accept Number From User & Display Pattern.c
@@ -0,0 +1,60 @@
+/*
+  
+  Input  :
+          Enter Number : 7846
+
+  Output : 
+          * * * * * * * 
+          * * * * * * * * 
+          * * * * 
+          * * * * * * 
+
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+//Compiler version gcc  6.3.0
+
+void Pattern(int iNo)
+{
+  int i=0,j=0,C=0,Rem=0,Rev=0,Temp=0;
+  
+  printf("\n");
+  for(Temp=iNo;Temp>0;Temp=Temp/10)
+  {
+    Rev=Temp%10+Rev*10;
+    C++;
+  }
+  
+  Temp=Rev;
+  
+  for(i=0;i<C;i++)
+  {
+    Rem=Temp%10;
+    
+    for(j=0;j<Rem;j++)
+    {
+      printf("* ");
+    }
+    
+    Temp=Temp/10;
+    printf("\n");
+  }
+  
+  return;
+}
+
+
+int main()
+{
+  int No;
+  
+  printf("Enter Number : ");
+  scanf("%d",&No);
+  
+  Pattern(No);
+  
+  getch();
+  return 0;
+}
